f53cdcf7cd1ce08e4a6284587802ab1ebd469c64
Win <erwincurato@gmail.com>
2026-08-07 12:11:17 +0800

    chore(config): add eslint and prettier tooling

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+const globals = require('globals');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  globalIgnores(['dist/*', 'IPSS_CodingGuide.jsx']),
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    files: ['metro.config.js', 'scripts/**/*.cjs'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+]);
